39c059429dbf3f0e2245946844d25be2a84c01de
shock <git@wdoughty.net>
2024-03-27 09:22:23 -0500

    initial Xcode project

---
+++ MetalGemini/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalGemini
+//
+//  Created by Bill Doughty on 3/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalGemini/MetalGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalGeminiApp.swift
+//  MetalGemini
+//
+//  Created by Bill Doughty on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
