8af3809de47a50d421dcb161288969411cd2a42e
shock <git@wdoughty.net>
2024-04-18 01:48:31 -0500

    Auto buffer alignment for float-float4 works

--- MetalGemini/UniformManager.swift
+++ MetalGemini/UniformManager.swift
@@ -19,50 +19,67 @@ extension Array where Element == Float
 
 class Float4Dictionary
 {
+    private var semaphore = DispatchSemaphore(value: 1) // Allows 1 concurrent access
     var map: [String: SIMD4<Float>] = [:]
 
     init() {}
 
     func setTuple( _ key: String, values: [Float])
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         let value = values.toSIMD4()
         map[key] = value
     }
 
     func set( _ key: String, _ simd4: SIMD4<Float> )
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         map[key] = simd4
     }
 
     func get( _ key: String, _ defaultValue: SIMD4<Float> = SIMD4<Float>(0,0,0,0) ) -> SIMD4<Float>
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         return map[key, default: defaultValue]
     }
 
     func getAsFloat( _ key: String, _ defaultValue: Float = 0 ) -> Float
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         guard let float4 = map[key] else { return defaultValue }
         return float4.x
     }
 
     func getAsFloat2( _ key: String, _ defaultValue: SIMD2<Float> = SIMD2<Float>(0,0)) -> SIMD2<Float>
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         guard let float4 = map[key] else { return defaultValue }
         return SIMD2<Float>(float4.x,float4.y)
     }
 
     func getAsFloat3( _ key: String, _ defaultValue: SIMD3<Float> = SIMD3<Float>(0,0,0)) -> SIMD3<Float>
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         guard let float4 = map[key] else { return defaultValue }
         return SIMD3<Float>(float4.x,float4.y,float4.z)
     }
 
     func delete( _ key: String ) -> SIMD4<Float>?
     {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         return map.removeValue(forKey: key)
     }
 
     func clear() {
+        semaphore.wait()  // wait until the resource is free to use
+        defer { semaphore.signal() }  // signal that the resource is free now
         map.removeAll()
     }
 }
@@ -109,17 +126,50 @@ class UniformManager
         dirty = true
     }
 
-    func mapUniformsToBuffer()
-    {
-        if( !dirty ) {return}
+    func mapUniformsToBuffer() {
+        if !dirty { return }
         dirty = false
-        if( debug ) { print("Updating uniforms buffer") }
+        if debug { print("Updating uniforms buffer") }
         guard let buffer = self.buffer else { return }
-        let bufferContents = buffer.contents().assumingMemoryBound(to: SIMD4<Float>.self)
-        for i in 0..<indexMap.count  {
-            let (key,type) = indexMap[i]
-            let float4 = float4dict.get(key)
-            bufferContents[i] = float4
+        let bufferContents = buffer.contents().bindMemory(to: Float.self, capacity: buffer.length / MemoryLayout<Float>.size)
+
+        var offset = 0
+        for i in 0..<indexMap.count {
+            let (key, dataType) = indexMap[i]
+            switch dataType {
+            case "float":
+                var data = float4dict.getAsFloat(key)
+                // Ensure the offset is aligned
+                offset = (offset + MemoryLayout<Float>.alignment - 1) / MemoryLayout<Float>.alignment * MemoryLayout<Float>.alignment
+                // Copy the data
+                memcpy(buffer.contents().advanced(by: offset), &data, MemoryLayout<Float>.size)
+                // Update the offset
+                offset += MemoryLayout<Float>.size
+            case "float2":
+                var data = float4dict.getAsFloat2(key)
+                // Ensure the offset is aligned
+                offset = (offset + MemoryLayout<SIMD2<Float>>.alignment - 1) / MemoryLayout<SIMD2<Float>>.alignment * MemoryLayout<SIMD2<Float>>.alignment
+                // Rebind memory and copy the data
+                memcpy(buffer.contents().advanced(by: offset), &data, MemoryLayout<SIMD3<Float>>.size)
+                // Update the offset
+                offset += MemoryLayout<SIMD2<Float>>.size
+            case "float3":
+                var data = float4dict.getAsFloat3(key)
+                // Ensure the offset is aligned
+                offset = (offset + MemoryLayout<SIMD3<Float>>.alignment - 1) / MemoryLayout<SIMD3<Float>>.alignment * MemoryLayout<SIMD3<Float>>.alignment
+                // Rebind memory and copy the data
+                memcpy(buffer.contents().advanced(by: offset), &data, MemoryLayout<SIMD3<Float>>.size)
+                // Update the offset
+                offset += MemoryLayout<SIMD3<Float>>.size
+            default: // Assuming default is "float4"
+                var data = float4dict.get(key)
+                // Ensure the offset is aligned
+                offset = (offset + MemoryLayout<SIMD4<Float>>.alignment - 1) / MemoryLayout<SIMD4<Float>>.alignment * MemoryLayout<SIMD4<Float>>.alignment
+                // Rebind memory and copy the data
+                memcpy(buffer.contents().advanced(by: offset), &data, MemoryLayout<SIMD4<Float>>.size)
+                // Update the offset
+                offset += MemoryLayout<SIMD4<Float>>.size
+            }
         }
     }
 
