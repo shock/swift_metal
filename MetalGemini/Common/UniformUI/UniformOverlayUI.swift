3b742b08392744f655ac355dbce2fb770f9e8b0f
shock <git@wdoughty.net>
2024-05-07 11:09:50 -0500

    LazyHStack to the rescue

--- MetalGemini/Common/UniformUI/UniformOverlayUI.swift
+++ MetalGemini/Common/UniformUI/UniformOverlayUI.swift
@@ -13,9 +13,15 @@ struct UniformOverlayUI: View {
 
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                ForEach(0..<viewModel.uniformVariables.count, id: \.self) { index in
-                    UniformControlView(viewModel: viewModel, variableIndex: index)
+            if viewModel.uniformVariables.count > 0 {
+                HStack {
+                    // since lazy doesn't establish geometry we have to establish it with one non-lazy view first
+                    UniformControlView(viewModel: viewModel, variableIndex: 0)
+                    LazyHStack {
+                        ForEach(1..<viewModel.uniformVariables.count, id: \.self) { index in
+                            UniformControlView(viewModel: viewModel, variableIndex: index)
+                        }
+                    }
                 }
             }
         }
